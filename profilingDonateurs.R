2857cd6d0c6785d25d8077257ff56d4412b6df76
LAMPIRIS\marc.wagner <marc.wagner@lampiris.be>
2020-01-20 17:06:22 +0100

    profiling donateurs data prep group address, names

--- profilingDonateurs.R
+++ profilingDonateurs.R
@@ -32,15 +32,30 @@ donateurs <- readRawDataFolder(path = paste( paste ( '..',  'Donateurs', sep='/'
 #end copy of main.R to load deduplicated codemandeurs
       
 #remove duplicates from dedupData
-      
-      
+coplaintiffs_clean <- dedupData[auto_reason!='D' | is.na(auto_reason),]      
+coplaintiffs_clean [,name_grouped:= paste(lastname, firstname, sep = ' ')]       
       
 #cleanup donateurs: split first name and last name
 check_data <- NULL
 donateurs[,is_complete:=1]
+# remove Mme, Mr, M Mevr etc if there are more than 2 spaces
+# remove BVBA etc... companies
+# build equivalent of address_validated
+
 cleandonateurs<-cleanRawData(donateurs, check_data)
-      
-      
-      
+cleandonateurs[ , address_validated := paste(ifelse(is.na(address), '', address )
+                                             ,ifelse(is.na(street_nb), '', street_nb )
+                                             ,ifelse(is.na(address2), '', address2 )
+                                             ,',' 
+                                             ,ifelse(is.na(zip), '', zip ) 
+                                             ,ifelse(is.na(locality), '', locality ) 
+                                             ,ifelse(is.na(country), '', country ) )]  
+
 #      
 #run recordlinkage on best info available between donateurs and codemandeurs
+
+
+
+
+
+#end
